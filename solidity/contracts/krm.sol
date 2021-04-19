3e26d392a996e034059a217922a78624c6bb0072
Alain Papazoglou <alain@kredeum.com>
2021-04-19 15:02:23 +0200

    deploy KRM burnable and mintable on goerli and mumbai

--- solidity/contracts/krm.sol
+++ solidity/contracts/krm.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KRM is ERC20 {
-  constructor(uint256 initialSupply) ERC20('Kredeum', 'KRM') {
-    _mint(msg.sender, initialSupply);
-  }
+contract KRM is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Kredeum", "KRM") {    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
