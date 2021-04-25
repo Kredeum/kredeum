d59ec8d2f69b7322c6ed3b2c19f64920710f4772
Alain Papazoglou <alain@kredeum.com>
2021-04-25 10:07:06 +0200

    some address change, and put exact same name for both coins

---
+++ scripts/deploy-mkrm.mjs
@@ -0,0 +1,13 @@
+import hre from 'hardhat';
+const { ethers } = hre;
+const ethscan = hre.network.config.ethscan;
+
+const factory = await ethers.getContractFactory("MKRM");
+const mkrm = await factory.deploy();
+console.log(`Contract  ${ethscan}/address/${mkrm.address}`);
+
+const tx = (await mkrm.deployed()).deployTransaction;
+console.log(`TX        ${ethscan}/tx/${tx.hash}`);
+
+console.log('mKRM deployed!');
+console.log(await mkrm.symbol());
