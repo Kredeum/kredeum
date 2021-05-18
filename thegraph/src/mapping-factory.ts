5c833b05b14801b4ca0162d09ef7700766033a95
Alain Papazoglou <alain@kredeum.com>
2021-05-18 22:13:42 +0200

    wp templating

--- thegraph/src/mapping-factory.ts
+++ thegraph/src/mapping-factory.ts
@@ -1,15 +1,11 @@
-import { log } from '@graphprotocol/graph-ts';
-import { OpenNFTs } from '../generated/templates';
-import { NewOpenNFTs as NewOpenNFTsEvent } from '../generated/OpenNFTsFactory/OpenNFTsFactory';
+import { log } from "@graphprotocol/graph-ts";
+import { OpenNFTs } from "../generated/templates";
+import { NewOpenNFTs as NewOpenNFTsEvent } from "../generated/OpenNFTsFactory/OpenNFTsFactory";
 
 export function handleNewOpenNFTs(event: NewOpenNFTsEvent): void {
   let clone = event.params.clone.toHexString();
   let creator = event.params.creator.toHexString();
   let address = event.address.toHexString();
-  log.debug(`NewOpenNFTs detected. Clone: {} | Creator: {} | Address: {}`, [
-    clone,
-    creator,
-    address,
-  ]);
+  log.debug(`NewOpenNFTs detected. Clone: {} | Creator: {} | Address: {}`, [clone, creator, address]);
   OpenNFTs.create(event.params.clone);
 }
