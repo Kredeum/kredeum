e606bb46e9715d8333efe56b854cb20ffff777e1
Alain Papazoglou <alain@kredeum.com>
2021-06-07 19:08:09 +0200

    refacto ajax and add ajax import

--- wordpress/kredeum-nfts/admin/ajax/ajax.js
+++ wordpress/kredeum-nfts/admin/ajax/ajax.js
@@ -6,17 +6,50 @@
  * @package kredeum/nfts
  */
 
-jQuery(document).ready(function () {
-  document.querySelector("kredeum-metamask")?.$on("address", function () {
-    var data = {
-      action: "address",
-      address: document.querySelector("kredeum-metamask").address,
-      security: document.querySelector("#knonce").getAttribute("value")
-    };
-    console.log("AJAX CALL", data);
+let knonce;
 
-    jQuery.post(ajaxurl, data, function (response) {
-      console.log("AJAX RESPONSE", response);
+function _ajax(data) {
+  data.security = knonce;
+  console.log("AJAX CALL", data);
+
+  jQuery.post(ajaxurl, data, function (response) {
+    console.log("AJAX RESPONSE", response);
+  });
+}
+
+jQuery(document).ready(function () {
+  knonce = document.querySelector("#knonce").getAttribute("value");
+
+  // ACTION GET_ADDRESS
+  const kredeumMetamask = document.querySelector("kredeum-metamask");
+  kredeumMetamask?.$on("address", function () {
+    _ajax({
+      action: "address",
+      address: kredeumMetamask.address
+    });
+  });
+
+  // ACTION MINT_TOKEN
+  document.querySelectorAll("kredeum-nft-mint").forEach(function (kredeumNftMint) {
+    kredeumNftMint?.$on("token", function (e) {
+      _ajax({
+        action: "token",
+        minted: JSON.stringify({
+          chain_id: e.detail.minted.chainId,
+          address: e.detail.minted.address,
+          token_id: e.detail.minted.tokenId
+        }),
+        pid: kredeumNftMint.pid
+      });
+    });
+  });
+
+  // ACTION IMPORT_NFT
+  const importNft = document.querySelectorAll("import-nft");
+  importNft?.$on("import", function (e) {
+    _ajax({
+      action: "import",
+      url: importNft.url
     });
   });
 });
--- wordpress/kredeum-nfts/admin/ajax/ajax.php
+++ wordpress/kredeum-nfts/admin/ajax/ajax.php
@@ -20,7 +20,43 @@ add_action(
 	100
 );
 
+/**
+ * Add action token
+ */
+add_action(
+	'wp_ajax_token',
+	function () {
+		check_ajax_referer( 'get-token-id', 'security' );
 
+		if ( isset( $_POST['pid'] ) && isset( $_POST['minted'] ) ) {
+
+			$pid    = sanitize_text_field( wp_unslash( $_POST['pid'] ) );
+			$minted = json_decode( sanitize_text_field( wp_unslash( $_POST['minted'] ) ) );
+			echo esc_html( var_dump( $minted ) );
+
+			if ( isset( $minted->chain_id ) ) {
+				$res = add_post_meta( $pid, '_kre_chain_id', $minted->chain_id, true );
+				echo esc_html( var_dump( $res ) );
+			}
+
+			if ( isset( $minted->address ) ) {
+				$res = add_post_meta( $pid, '_kre_address', $minted->address, true );
+				echo esc_html( var_dump( $res ) );
+			}
+
+			if ( isset( $minted->token_id ) ) {
+				$res = add_post_meta( $pid, '_kre_token_id', $minted->token_id, true );
+				echo esc_html( var_dump( $res ) );
+			}
+		};
+
+		wp_die();
+	}
+);
+
+/**
+ * Add action address
+ */
 add_action(
 	'wp_ajax_address',
 	function () {
@@ -31,8 +67,27 @@ add_action(
 			update_user_meta( $user_id, 'ADDR', sanitize_text_field( wp_unslash( $_POST['address'] ) ) );
 		}
 
-		echo esc_html( sanitize_text_field( wp_unslash( $_POST['address'] ) ) );
-		echo ' => ' . esc_html( get_user_meta( $user_id, 'ADDR' )[0] );
+		echo esc_html( get_user_meta( $user_id, 'ADDR' )[0] );
+
+		wp_die();
+	}
+);
+
+
+/**
+ * Add action import
+ */
+add_action(
+	'wp_ajax_import',
+	function () {
+		check_ajax_referer( 'get-import', 'security' );
+
+		if ( isset( $_POST['url'] ) ) {
+			$pid = import( url );
+		}
+
+		echo esc_html( get_post_meta( '_source_url' )[0] );
+
 		wp_die();
 	}
 );
