0f9e058c5e0f1df3aacd8707e5c76bb094ff7ff2
Baptiste <baptiste@agence-polux.fr>
2021-11-02 21:23:56 +0100

    Header dapp

--- design-dapp/src/js/front.js
+++ design-dapp/src/js/front.js
@@ -25,4 +25,28 @@
             document.getElementById(tableDrop).style.height = "auto";
         }
     }
+
+    document.querySelector('.select-wrapper').addEventListener('click', function() {
+        this.querySelector('.select').classList.toggle('open');
+    })
+
+    for (const option of document.querySelectorAll(".custom-option")) {
+        option.addEventListener('click', function() {
+            if (!this.classList.contains('selected')) {
+                this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+                this.classList.add('selected');
+                this.closest('.select').querySelector('.select-trigger span').textContent = this.textContent;
+                this.closest('.select').querySelector('.select-trigger span').className = "";
+                this.closest('.select').querySelector('.select-trigger span').classList.add(this.dataset.value);
+            }
+        })
+    }
+
+    window.addEventListener('click', function(e) {
+        const select = document.querySelector('.select')
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    });
+
 })();
