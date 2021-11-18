678648c96499e3f79f94f637912b17be89075502
Baptiste <baptiste@agence-polux.fr>
2021-11-18 14:35:48 +0100

    Ajustements div id

--- design-dapp/src/js/front.js
+++ design-dapp/src/js/front.js
@@ -26,9 +26,11 @@
         }
     }
 
-    document.querySelector('.select-wrapper').addEventListener('click', function() {
-        this.querySelector('.select').classList.toggle('open');
-    })
+    for (const dropdown of document.querySelectorAll(".select-wrapper")) {
+        dropdown.addEventListener('click', function() {
+            this.querySelector('.select').classList.toggle('open');
+        })
+    }
 
     for (const option of document.querySelectorAll(".custom-option")) {
         option.addEventListener('click', function() {
@@ -43,9 +45,10 @@
     }
 
     window.addEventListener('click', function(e) {
-        const select = document.querySelector('.select')
-        if (!select.contains(e.target)) {
-            select.classList.remove('open');
+        for (const select of document.querySelectorAll('.select')) {
+            if (!select.contains(e.target)) {
+                select.classList.remove('open');
+            }
         }
     });
 
