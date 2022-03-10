2472aadb0eeb4eaa18228489a2eca5f4686c1e4b
Baptiste <baptiste@agence-polux.fr>
2022-03-10 14:29:42 +0100

    Delete function js

--- gulp/js/front.js
+++ gulp/js/front.js
@@ -1,21 +1,4 @@
 (function() {
-    
-	function copyText(element) {
-		var $copyText = document.getElementById(element).innerText;
-		var button = document.getElementById(element + '-button');
-		
-		navigator.clipboard.writeText($copyText).then(function() {
-			var originalText = button.innerText;
-			button.innerText = 'Copied!';
-			setTimeout(function(){
-				button.innerText = originalText;
-			}, 750);
-		}, function() {
-			button.style.cssText = "background-color: var(--red);";
-			button.innerText = 'Error';
-		});
-	}
-
     for (const dropdown of document.querySelectorAll(".select-wrapper")) {
         dropdown.addEventListener('click', function() {
             this.querySelector('.select').classList.toggle('open');
@@ -41,5 +24,4 @@
             }
         }
     });
-
 })();
