49297f26547577313bb705fee28624eb1425b911
dlaxcess <flipiste@free.fr>
2022-05-09 11:15:57 +0200

    base of mediasContainer component

---
+++ svelte/components/Global/mediasDisplay/MediasContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { NftType } from "lib/ktypes";
+
+  export let nft: NftType;
+  export let MIMEType: string = "image";
+  export let MIMESubtype: string = "jpeg";
+  export let imgLink: string = "";
+
+  $: mediaContentType = nft.contentType?.split("/");
+  $: MIMEType = mediaContentType[0];
+  $: MIMESubtype = mediaContentType[1];
+</script>
