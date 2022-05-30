05e66a25a270286eb71856bf4b942a3e70cfaaf5
dlaxcess <flipiste@free.fr>
2022-05-30 15:36:38 +0200

    fix firefox audioplayer display bug

--- svelte/components/Media/MediaDisplayAudio.svelte
+++ svelte/components/Media/MediaDisplayAudio.svelte
@@ -40,92 +40,68 @@
 
 <div class="audio-cover-image {small ? '' : 'audioDeployed'}">
   <MediaDisplayImage {mediaSrc} {alt} />
-  {#if small}
-    {#if "list" === displayMode}
-      <audio preload="none" bind:paused src={animation_url}>
-        <track kind="captions" />
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
-      <button on:click={playAudio} class="krd-play-audio-button krd-play-audio-list-button">
-        <i class="fa {paused ? 'fa-play-circle' : 'fa-pause-circle'} video-play-icon" />
-      </button>
-    {:else}
-      <!-- <audio controls preload="none" bind:this={player} src={animation_url}> -->
-      <audio controls preload="none" bind:paused src={animation_url}>
-        <!-- <audio controls preload="none" src={animation_url}> -->
-        <track kind="captions" />
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
-      <button on:click={playAudio} class="krd-play-audio-button">
-        <i class="fa {paused ? 'fa-play-circle' : 'fa-pause-circle'} video-play-icon" />
-      </button>
-    {/if}
-  {:else}
-    <audio controls autoplay src={animation_url}>
+</div>
+{#if small}
+  {#if "list" === displayMode}
+    <audio preload="none" bind:paused src={animation_url}>
+      <track kind="captions" />
       Your browser does not support the
       <code>audio</code> element.
     </audio>
+    <button on:click={playAudio} class="krd-play-audio-button krd-play-audio-list-button">
+      <i class="fa {paused ? 'fa-play-circle' : 'fa-pause-circle'} video-play-icon" />
+    </button>
+  {:else}
+    <!-- <audio controls preload="none" bind:this={player} src={animation_url}> -->
+    <audio controls preload="none" bind:paused src={animation_url}>
+      <!-- <audio controls preload="none" src={animation_url}> -->
+      <track kind="captions" />
+      Your browser does not support the
+      <code>audio</code> element.
+    </audio>
+    <button on:click={playAudio} class="krd-play-audio-button">
+      <i class="fa {paused ? 'fa-play-circle' : 'fa-pause-circle'} video-play-icon" />
+    </button>
   {/if}
-</div>
+{:else}
+  <audio controls autoplay src={animation_url}>
+    Your browser does not support the
+    <code>audio</code> element.
+  </audio>
+{/if}
 
 <style>
-  /* audio::-webkit-media-controls-panel,
-  audio::-webkit-media-controls-mute-button,
-  audio::-webkit-media-controls-play-button,
-  audio::-webkit-media-controls-timeline-container,
-  audio::-webkit-media-controls-current-time-display,
-  audio::-webkit-media-controls-time-remaining-display,
-  audio::-webkit-media-controls-timeline,
-  audio::-webkit-media-controls-volume-slider-container,
-  audio::-webkit-media-controls-volume-slider,
-  audio::-webkit-media-controls-seek-back-button,
-  audio::-webkit-media-controls-seek-forward-button,
-  audio::-webkit-media-controls-fullscreen-button,
-  audio::-webkit-media-controls-rewind-button,
-  audio::-webkit-media-controls-return-to-realtime-button,
-  audio::-webkit-media-controls-toggle-closed-captions-button {
-     color: white !important;
-    filter: invert(100%);
-    background-color: white;
-  }  */
-
   audio::-webkit-media-controls-enclosure {
     border-radius: 6px;
     background-color: rgba(0, 0, 0, 0.5);
   }
 
   audio::-webkit-media-controls-panel {
-    /* color: white !important; */
     filter: invert(100%);
   }
 
-  /* audio::-webkit-media-controls-mute-button {
-    color: white !important;
-  } */
-
-  /* audio::-webkit-media-controls-timeline {
-    color: white;
-  } */
-
   .audio-cover-image {
-    /* position: inherit; */
-    /* top: 0;
-    left: 0; */
     width: 100%;
     height: 100%;
   }
 
-  .audio-cover-image:not(.audioDeployed) audio {
+  .audio-cover-image:not(.audioDeployed) + audio {
     position: absolute;
     bottom: 0;
     left: 50%;
     transform: translate(-50%, 0%);
     width: 100%;
+    height: 54px;
   }
 
-  .audioDeployed audio {
+  @supports (-moz-appearance: none) {
+    .audio-cover-image:not(.audioDeployed) + audio {
+      padding: 0px 0 7px 0;
+      background-color: rgba(0, 0, 0, 1);
+    }
+  }
+
+  .audioDeployed + audio {
     transform: translate(50%, 15%);
   }
 
@@ -148,7 +124,12 @@
   }
 
   .krd-play-audio-list-button {
-    bottom: 12px;
-    left: 2%;
+    top: 65px;
+    left: 35px;
+    bottom: unset;
+  }
+
+  .krd-play-audio-list-button i {
+    border: 1px solid lightgray;
   }
 </style>
