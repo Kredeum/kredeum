fb89a46acf0c81ebdf56d16e2f54d1234e041789
dlaxcess <flipiste@free.fr>
2022-06-07 17:09:57 +0200

    wip

--- common/lib/knft-get-metadata.ts
+++ common/lib/knft-get-metadata.ts
@@ -41,8 +41,8 @@ const nftGetContentType = async (nft: NftType): Promise<string> => {
     } catch (e) {
       console.error("ERROR nftGetContentType", e, url, nft);
     }
+    // console.log(`nftGetContentType ${nftKey(chainId, address, tokenID)}\n`, url, contentType);
   }
-  // console.log(`nftGetContentType ${nftKey(chainId, address, tokenID)}\n`, url, contentType);
 
   return contentType;
 };
