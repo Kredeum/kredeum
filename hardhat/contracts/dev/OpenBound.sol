6b1fa87125d8e45ddedd705e4360918257f3577a
zapaz.eth <alain@kredeum.com>
2022-06-08 13:12:09 +0200

    refacto sol contracts

--- hardhat/contracts/dev/OpenBound.sol
+++ hardhat/contracts/dev/OpenBound.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../interfaces/IERC173.sol";
-import "./interfaces/IERC4973.sol";
-import "./interfaces/IERC721Enumerable.sol";
-import "./interfaces/IERC721Metadata.sol";
 import "./interfaces/IOpenBound.sol";
+import "../interfaces/IERC4973.sol";
+import "../interfaces/IERC173.sol";
+import "../interfaces/IERC721Enumerable.sol";
+import "../interfaces/IERC721Metadata.sol";
 import "./library/Bafkrey.sol";
 
 /// @title OpenBound smartcontract
-contract OpenBound is IOpenBound, IERC4973, IERC721Enumerable, IERC721Metadata, IERC173 {
+contract OpenBound is IOpenBound, IERC4973, IERC173, IERC721Enumerable, IERC721Metadata {
     address public owner;
 
     string public name;
     string public symbol;
     uint256 public totalSupply;
 
-    mapping(uint256 => address) _owners;
-    mapping(address => uint256[]) _tokensOfOwner;
-    uint256[] _tokens;
+    mapping(uint256 => address) internal _owners;
+    mapping(address => uint256[]) internal _tokensOfOwner;
+    uint256[] internal _tokens;
 
-    string private _BASE_URI = "ipfs://";
+    string private constant _BASE_URI = "ipfs://";
 
     modifier onlyOwner() {
         require((owner == msg.sender), "Not owner");
@@ -32,12 +32,41 @@ contract OpenBound is IOpenBound, IERC4973, IERC721Enumerable, IERC721Metadata, 
         symbol = symbol_;
     }
 
-    function mint(address addr, uint256 tokenID) public onlyOwner {
+    function transferOwnership(address newOwner) external override(IERC173) {
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+    function ownerOf(uint256 tokenID) external view override(IERC4973) returns (address nftOwner) {
+        nftOwner = _owners[tokenID];
+    }
+
+    function tokenByIndex(uint256 index) external view override(IERC721Enumerable) returns (uint256) {
+        return _tokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address addr, uint256 index)
+        external
+        view
+        override(IERC721Enumerable)
+        returns (uint256)
+    {
+        return _tokensOfOwner[addr][index];
+    }
+
+    function mint(address addr, uint256 tokenID) public override(IOpenBound) onlyOwner {
         _mint(addr, tokenID);
     }
 
-    function mint(uint256 tokenID) public {
+    function mint(uint256 tokenID) public override(IOpenBound) {
         _mint(msg.sender, tokenID);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(IERC721Metadata) returns (string memory) {
+        require(_exists(tokenId), "ERC721: token doesn't exists");
+
+        return string(abi.encodePacked(_BASE_URI, Bafkrey.uint256ToCid(tokenId)));
     }
 
     function _mint(address addr, uint256 tokenID) internal {
@@ -48,31 +77,7 @@ contract OpenBound is IOpenBound, IERC4973, IERC721Enumerable, IERC721Metadata, 
         _tokensOfOwner[addr].push(tokenID);
     }
 
-    function ownerOf(uint256 tokenID) external view returns (address nftOwner) {
-        nftOwner = _owners[tokenID];
-    }
-
-    function tokenURI(uint256 tokenId) public view override(IERC721Metadata) returns (string memory) {
-        require(_exists(tokenId), "ERC721: token doesn't exists");
-
-        return string(abi.encodePacked(_BASE_URI, Bafkrey.uint256ToCid(tokenId)));
-    }
-
-    function tokenByIndex(uint256 index) external view returns (uint256) {
-        return _tokens[index];
-    }
-
-    function tokenOfOwnerByIndex(address addr, uint256 index) external view returns (uint256) {
-        return _tokensOfOwner[addr][index];
-    }
-
     function _exists(uint256 tokenID) internal view returns (bool) {
         return _owners[tokenID] != address(0);
-    }
-
-    function transferOwnership(address newOwner) external {
-        address oldOwner = owner;
-        owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
     }
 }
