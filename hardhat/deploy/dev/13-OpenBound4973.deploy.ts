ac5c74cb560e1fbf9da5917289e4e8baac39aaff
zapaz.eth <alain@kredeum.com>
2022-06-08 20:34:39 +0200

    ERC721 OpenBound

--- hardhat/deploy/dev/13-OpenBound4973.deploy.ts
+++ hardhat/deploy/dev/13-OpenBound4973.deploy.ts
@@ -1,13 +1,13 @@
 import type { DeployFunction } from "hardhat-deploy/types";
 
-const contractName = "OpenBound";
+const contractName = "OpenBound4973";
 
 const deployFunction: DeployFunction = async function ({ deployments, ethers }) {
   const deployer = await ethers.getNamedSigner("deployer");
 
   await deployments.deploy(contractName, {
     from: deployer.address,
-    args: ["OpenBound", "BOUND"],
+    args: [contractName, "B4973"],
     log: true
   });
 };
