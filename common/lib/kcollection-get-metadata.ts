812ddf0b2b6312e96326eafad5d794008361f344
zapaz.eth <alain@kredeum.com>
2022-08-19 15:34:46 +0200

    delete semaphore, too hard to maintain

--- common/lib/kcollection-get-metadata.ts
+++ common/lib/kcollection-get-metadata.ts
@@ -5,12 +5,10 @@ import { collectionGetSupportsNew } from "./kcollection-get-supports-new";
 import { collectionGetSupportsOld } from "./kcollection-get-supports-old";
 import { collectionGetOtherDataOld } from "./kcollection-get-other-data-old";
 
-import Semaphore from "semaphore-async-await";
 import { resolverGetAddress } from "./kresolver-get";
 
 // Cache supports   (chainId, address)
 const supportsCache: Map<string, CollectionSupports> = new Map();
-const locks: Map<string, Semaphore> = new Map();
 
 const collectionGetSupports = async (
   chainId: number,
@@ -22,37 +20,24 @@ const collectionGetSupports = async (
 
   let supports: CollectionSupports | undefined = {};
 
-  // console.log(`collectionGetSupports IN ${collectionKey(chainId, address)}`);
+  console.log(`collectionGetSupports IN ${collectionKey(chainId, address)}`);
 
-  const lockPrevious = locks.get(collectionKey(chainId, address));
-  if (lockPrevious) {
-    await lockPrevious.acquire();
-    supports = supportsCache.get(collectionKey(chainId, address));
-    lockPrevious.release();
+  supports = supportsCache.get(collectionKey(chainId, address));
 
-    if (!supports?.IERC165) console.error("collectionGetSupports ERROR IERC165");
-    return supports || {};
+  if (!supports) {
+    if (resolverGetAddress(chainId)) {
+      supports = await collectionGetSupportsNew(chainId, address, provider);
+    } else {
+      supports = await collectionGetSupportsOld(chainId, address, provider);
+    }
+
+    for (const key in supports) {
+      if (!supports[key as ABIS]) delete supports[key as ABIS];
+    }
+    supportsCache.set(collectionKey(chainId, address), supports);
   }
 
-  const lock = new Semaphore(1);
-  locks.set(collectionKey(chainId, address), lock);
-  await lock.acquire();
-
-  if (resolverGetAddress(chainId)) {
-    supports = await collectionGetSupportsNew(chainId, address, provider);
-  } else {
-    supports = await collectionGetSupportsOld(chainId, address, provider);
-  }
-
-  for (const key in supports) {
-    if (!supports[key as ABIS]) delete supports[key as ABIS];
-  }
-
-  // console.log(`collectionGetSupports OUT ${collectionKey(chainId, address)}\n`, supports);
-
-  // supportsCache.set(collectionKey(chainId, address), supports);
-  // lock.release();
-
+  console.log(`collectionGetSupports OUT ${collectionKey(chainId, address)}\n`, supports);
   return supports;
 };
 
