7cc6d8dec619cb9f64b5ca25b893bcce2327f35d
zapaz.eth <alain@kredeum.com>
2022-09-08 15:26:46 +0200

    burn example

--- common/lib/nft/kburn.ts
+++ common/lib/nft/kburn.ts
@@ -2,8 +2,10 @@ import type { IERC721 } from "@soltypes/OpenNFTs/contracts/interfaces/IERC721";
 import type { IERC1155 } from "@soltypes/OpenNFTs/contracts/interfaces/IERC1155";
 import type { JsonRpcSigner, TransactionResponse, TransactionReceipt } from "@ethersproject/providers";
 
-import { collectionGetContract } from "@lib/collection/kcollection-get";
+import { collectionGetContract, collectionBurnable } from "@lib/collection/kcollection-get";
 import { explorerTxLog } from "../common/kconfig";
+
+const AddressdEaD = "0x000000000000000000000000000000000000dEaD";
 
 async function* burnNft(
   chainId: number,
@@ -11,64 +13,21 @@ async function* burnNft(
   tokenID: string,
   signer: JsonRpcSigner
 ): AsyncGenerator<TransactionResponse | TransactionReceipt | Record<string, never>> {
-  // console.log("transferNft", chainId, address, tokenID, to);
-
-  let txResp: TransactionResponse | undefined;
-
-  if (!(chainId && address && tokenID && signer)) return txResp;
-
   const account = await signer.getAddress();
+  console.log("burnNft", chainId, address, tokenID, account);
 
-  const { contract, collection } = await collectionGetContract(chainId, address, signer);
-  // console.log("contract", contract);
+  if (!(chainId && address && tokenID && account)) return {};
 
-  const burnOpenNFT = "burn(uint256)";
+  const { contract } = await collectionGetContract(chainId, address, signer);
 
-  if (contract[burnOpenNFT] && !collection.open && account === collection.owner) {
-    const burnFunction = contract[burnOpenNFT] as {
-      (tokenID: string): Promise<TransactionResponse>;
-    };
-    txResp = await burnFunction(tokenID);
-  }
-  if (!txResp) return {};
+  const burnFunction = await collectionBurnable(chainId, address, signer);
+  if (!burnFunction) return {};
+
+  const txResp = await contract[burnFunction](tokenID);
   explorerTxLog(chainId, txResp);
 
   yield txResp;
   yield await txResp.wait();
 }
 
-async function* burnNftAddressDead(
-  chainId: number,
-  address: string,
-  tokenID: string,
-  signer: JsonRpcSigner
-): AsyncGenerator<TransactionResponse | TransactionReceipt | Record<string, never>> {
-  // console.log("transferNft", chainId, address, tokenID, to);
-
-  if (!(chainId && address && tokenID && signer)) return {};
-
-  const fromAddress = await signer.getAddress();
-  // console.log("transferNft from", fromAddress);
-  const AddressdEaD = "0x000000000000000000000000000000000000dEaD";
-
-  const { contract, collection } = await collectionGetContract(chainId, address, signer);
-  // console.log("contract", contract);
-
-  let txResp: TransactionResponse | undefined;
-  if (collection.supports?.IERC721) {
-    txResp = await (contract as IERC721)["safeTransferFrom(address,address,uint256)"](
-      fromAddress,
-      AddressdEaD,
-      tokenID
-    );
-  } else if (collection.supports?.IERC1155) {
-    txResp = await (contract as IERC1155).safeTransferFrom(fromAddress, AddressdEaD, tokenID, 1, "0x00");
-  }
-  if (!txResp) return {};
-  explorerTxLog(chainId, txResp);
-
-  yield txResp;
-  yield await txResp.wait();
-}
-
-export { burnNft, burnNftAddressDead };
+export { burnNft, AddressdEaD };
--- svelte/components/Nft/NftBurn.svelte
+++ svelte/components/Nft/NftBurn.svelte
@@ -1,77 +1,166 @@
 <script lang="ts">
-  import { getContext } from "svelte";
+  import { getContext, onMount } from "svelte";
   import { Writable } from "svelte/store";
 
-  import { burnNft, burnNftAddressDead } from "@lib/nft/kburn";
+  import { burnNft, AddressdEaD } from "@lib/nft/kburn";
   import { explorerNftUrl, explorerTxUrl, textShort } from "@lib/common/kconfig";
+  import { collectionBurnable } from "@lib/collection/kcollection-get";
+  import { transferNft } from "@lib/nft/ktransfer";
 
   import { metamaskChainId, metamaskSigner } from "@main/metamask";
-
   import { nftStore } from "@stores/nft/nft";
 
   /////////////////////////////////////////////////
-  //  <NftBurn {chainId} {address} {tokenID} />
-  // Display NFT
+  // <NftBurn {chainId} {address} {tokenID} />
+  // Burn NFT
   /////////////////////////////////////////////////
   export let chainId: number;
   export let address: string;
   export let tokenID: string;
+  /////////////////////////////////////////////////
 
-  let burnTxHash: string = null;
-  let burning = false;
-  let burned = false;
-  let burnImplossible = false;
-  let burnImplossibleAddressDead = false;
+  let burning: number = 0;
+  let burnable: boolean;
+  let burnTxHash: string;
+  let error: string;
 
   // Context for refreshCollectionList
-  ///////////////////////////////////////////////////////////
   let refreshCollectionList: Writable<number> = getContext("refreshCollectionList");
-  ///////////////////////////////////////////////////////////
 
-  const burn = async (dEaDBurn = false) => {
-    if ($metamaskSigner) {
-      burnTxHash = null;
-      burnImplossible = false;
-      burning = true;
-      burned = false;
-
-      const txRespYield = dEaDBurn
-        ? burnNftAddressDead(chainId, address, tokenID, $metamaskSigner)
-        : burnNft(chainId, address, tokenID, $metamaskSigner);
-
-      const txResp = (await txRespYield.next()).value;
-
-      if (txResp.hash) {
-        burnTxHash = txResp.hash;
-        const txReceipt = (await txRespYield.next()).value;
-
-        burned = Boolean(txReceipt.status);
-        burning = false;
-
-        nftStore.nftRemoveOne(chainId, address, tokenID);
-        $refreshCollectionList += 1;
-      } else {
-        dEaDBurn ? (burnImplossibleAddressDead = true) : (burnImplossible = true);
-      }
-    }
+  const _error = (err: string): void => {
+    error = err;
+    console.error(error);
+    burning = 0;
   };
 
-  const returnNftsList = () => {
-    if (burned) {
-    }
+  // BURNING STATE
+  //
+  //  STATE 0 Start
+  //    |
+  //  TEST burnable
+  //    |         \
+  //  STATE 1    STATE 2
+  //  Confirm    Confirm
+  //   Burn    Transfer2Dead
+  //    |         /
+  //  STATE 3
+  // Ask for signature
+  //    |
+  //  STATE 4  Sending TX
+  //    |
+  //  TEST TxResp --> ERROR sending TX
+  //    |
+  //  STATE 5 Display TX Hash
+  //    |
+  //  TEST TxReceipt --> ERROR inside TX
+  //    |
+  //  STATE 6 End TX & Refresh
+  //    |
+  //  CLICK Close
+  //    |
+  //  STATE 0 popup closed
+
+  // STATES : S0 -S6
+  const S0_START = 0;
+  const S1_CONFIRM_BURN = 1;
+  const S2_CONFIRM_TRANSFER = 2;
+  const S3_SIGN_TX = 3;
+  const S4_SENT_TX = 4;
+  const S5_WAIT_TX = 5;
+  const S6_BURNED = 6;
+
+  const burnInit = async () => {
+    burnable = false;
+    burnTxHash = null;
+
+    burning = S0_START;
+
+    burnable = Boolean(await collectionBurnable(chainId, address, $metamaskSigner));
+
+    burning = burnable ? S1_CONFIRM_BURN : S2_CONFIRM_TRANSFER;
   };
+
+  const burnConfirm = async () => {
+    let txRespYield = burnable
+      ? burnNft(chainId, address, tokenID, $metamaskSigner)
+      : transferNft(chainId, address, tokenID, $metamaskSigner, AddressdEaD);
+
+    burning = S3_SIGN_TX;
+
+    const txResp = (await txRespYield.next()).value;
+    
+    burning = S4_SENT_TX;
+    
+    burnTxHash = txResp?.hash;
+    if (!burnTxHash) return _error(`ERROR while sending transaction... ${JSON.stringify(txResp, null, 2)}`);
+
+    burning = S5_WAIT_TX;
+
+    const txReceipt = (await txRespYield.next()).value;
+
+    if (!Boolean(txReceipt.status)) return _error(`ERROR returned by transaction ${txReceipt}`);
+
+    burning = S6_BURNED;
+
+    nftStore.nftRemoveOne(chainId, address, tokenID);
+    $refreshCollectionList += 1;
+  };
+
+  onMount(() => {
+    burnInit();
+  });
 </script>
 
 <div id="kredeum-burn-nft">
   <div class="modal-content">
-    <a href="./#" title="Close" on:click={returnNftsList} class="modal-close"><i class="fa fa-times" /></a>
+    <a href="./#" title="Close" on:click={() => (burning = S0_START)} class="modal-close"><i class="fa fa-times" /></a>
 
     <div class="modal-body">
       <div class="titre">
         <i class="fas fa-fire" /> Burn this NFT #{tokenID} Forever ?
       </div>
       <div>
-        {#if burned}
+        {#if burning == S0_START}
+          <div class="section">Checking contract...</div>
+        {:else if burning == S1_CONFIRM_BURN}
+          <div class="section">
+            <div class="form-field kre-burn-warning">
+              <p>
+                <i class="fas fa-exclamation-triangle fa-left c-red" /> Be carefull, you're about to burn this NFT #{tokenID}
+              </p>
+              <p>This operation is irreversible !!!</p>
+            </div>
+          </div>
+
+          <div class="txtright">
+            <button class="btn btn-default btn-sell" type="submit" on:click={() => burnConfirm()}
+              >Yes, I want to burn</button
+            >
+          </div>
+        {:else if burning == S2_CONFIRM_TRANSFER}
+          <div class="section">
+            <div class="form-field kre-burn-warning">
+              <p>
+                <i class="fas fa-exclamation-triangle fa-left c-red" /> This NFT #{tokenID} can't be burned!
+              </p>
+              <p>But you can get rid of it anyway, by transfering it unusable "dEaD address"</p>
+              <p>{AddressdEaD}</p>
+              <p>This operation is irreversible !!!</p>
+            </div>
+          </div>
+
+          <div class="txtright">
+            <button class="btn btn-default btn-sell" type="submit" on:click={() => burnConfirm()}
+              >Yes, I want to transfer it</button
+            >
+          </div>
+        {:else if burning == S3_SIGN_TX}
+          <div class="section">Please, sign the transaction</div>
+        {:else if burning == S4_SENT_TX}
+          <div class="section">Sending transaction...</div>
+        {:else if burning == S5_WAIT_TX}
+          <div class="section">Wait till completed, it may take one minute or more.</div>
+        {:else if burning == S6_BURNED}
           <div>
             <div class="titre">
               <i class="fas fa-check fa-left c-green" />
@@ -79,54 +168,8 @@
               <a class="link" href="{explorerNftUrl(chainId, { chainId, address, tokenID })}}" target="_blank"
                 >#{tokenID}</a
               >
-              burned!
+              {burnable ? "burned" : "transfered"}!
             </div>
-          </div>
-        {:else if burnImplossible}
-          <div class="section">
-            <div class="form-field kre-burn-warning">
-              <p>
-                <i class="fas fa-exclamation-triangle fa-left c-red" /> NFT can't be burned with normalized burn function!
-              </p>
-              <p>Do you want to burn it afterall buy transfer it at "address dEaD"</p>
-            </div>
-          </div>
-          <div class="txtright">
-            <button class="btn btn-default btn-sell" type="submit" on:click={() => burn(true)}
-              >Yes, burn to address dEaD</button
-            >
-          </div>
-        {:else if burnImplossibleAddressDead}
-          <div class="section">
-            <div class="form-field kre-burn-warning">
-              <p>
-                <i class="fas fa-exclamation-triangle fa-left c-red" /> NFT can't be burned !
-              </p>
-            </div>
-          </div>
-        {:else if burning}
-          <div class="titre">
-            <i class="fas fa-sync fa-left c-green" />burning NFT...
-          </div>
-          <div class="section">
-            {#if burnTxHash}
-              Wait till completed, it may take one minute or more.
-            {:else}
-              Sign the transaction
-            {/if}
-          </div>
-        {:else}
-          <div class="section">
-            <div class="form-field kre-burn-warning">
-              <p>
-                <i class="fas fa-exclamation-triangle fa-left c-red" /> Be carefull, you're about to burn this NFT #{tokenID}
-              </p>
-              <p>this operation is irreversible !!!</p>
-            </div>
-          </div>
-
-          <div class="txtright">
-            <button class="btn btn-default btn-sell" type="submit" on:click={() => burn()}>Yes, I want to burn</button>
           </div>
         {/if}
         {#if burnTxHash}
@@ -135,6 +178,9 @@
               >{textShort(burnTxHash)}</a
             >
           </div>
+        {/if}
+        {#if error}
+          <div class="section">{error}</div>
         {/if}
       </div>
     </div>
