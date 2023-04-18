8f6decf07d6c24111f3367e34a968de643e162d6
dlaxcess <flipiste@free.fr>
2023-04-18 21:03:47 +0200

    display IPFS or Swarm option depending on STORAGE_CHOICE

--- wordpress/plugins/kredeum-nfts/admin/settings/fields.php
+++ wordpress/plugins/kredeum-nfts/admin/settings/fields.php
@@ -11,64 +11,77 @@ namespace KredeumNFTs\Settings;
  * Kredeum NFTs fields definition
  */
 function fields() {
-	 return array(
-
-		 array(
-			 'uid'     => 'select_collection',
-			 'label'   => 'Connect Metamask and select Network and Collection to Mint',
-			 'section' => 'first_section',
-			 'type'    => 'kcollections',
-			 'default' => '',
-		 ),
-
-		 array(
-			 'uid'     => 'storage_auto',
-			 'label'   => 'STORAGE_AUTO',
-			 'default' => '1',
-			 'section' => 'first_section',
-			 'type'    => 'select',
-			 'options' => array( __( 'No', 'kredeum-nfts' ), __( 'Yes', 'kredeum-nfts' ) ),
-			 'helper'  => __( 'Choose "yes" to automaticaly push your new medias to decentralized storage on upload', 'kredeum-nfts' ),
-		 ),
-
-		 array(
-			 'uid'     => 'storage_choice',
-			 'label'   => 'STORAGE_CHOICE',
-			 'default' => '1',
-			 'section' => 'first_section',
-			 'type'    => 'select',
-			 'options' => array( 'IPFS' => __( 'IPFS', 'kredeum-nfts' ), 'SWARM' => __( 'Swarm', 'kredeum-nfts' ) ),
-			 'helper'  => __( 'Choose on witch decentralized storage you want to upload your files', 'kredeum-nfts' ),
-		 ),
-
-		 array(
-			 'uid'         => 'nft_storage_key',
-			 'label'       => 'NFT_STORAGE_KEY',
-			 'section'     => 'first_section',
-			 'type'        => 'textarea',
-			 'placeholder' => 'NFT Storage key',
-			 'default'     => '',
-			 'helper'      => __( 'Enter your own NFT Storage Key, or leave blank to use common key', 'kredeum-nfts' ),
-		 ),
-		 
-		 array(
-			'uid'         => 'swarm_node_url',
-			'label'       => 'SWARM_NODE_URL',
-			'section'     => 'first_section',
-			'type'        => 'text',
-			'placeholder' => 'Your Swarm Bee node URL',
-			'default'     => '',
-			'helper'      => __( 'Enter your own Swarm Bee node Url (ex: http://localhost:1633), or leave blank to use Swarm free(limited) Gateway', 'kredeum-nfts' ),
-		),
+	$fields = array(
 		array(
-			'uid'         => 'swarm_batch_id',
-			'label'       => 'SWARM_BATCH_ID',
-			'section'     => 'first_section',
-			'type'        => 'text',
-			'placeholder' => 'Your Swarm Bee Batch of stamps ID',
-			'default'     => '',
-			'helper'      => __( 'Enter your own Swarm Bee Batch of stamps ID, or leave blank to use Swarm free(limited) Gateway', 'kredeum-nfts' ),
+			'uid'     => 'select_collection',
+			'label'   => 'Connect Metamask and select Network and Collection to Mint',
+			'section' => 'first_section',
+			'type'    => 'kcollections',
+			'default' => '',
 		),
 
-	 );
+		array(
+			'uid'     => 'storage_auto',
+			'label'   => 'STORAGE_AUTO',
+			'default' => '1',
+			'section' => 'first_section',
+			'type'    => 'select',
+			'options' => array( __( 'No', 'kredeum-nfts' ), __( 'Yes', 'kredeum-nfts' ) ),
+			'helper'  => __( 'Choose "yes" to automaticaly push your new medias to decentralized storage on upload', 'kredeum-nfts' ),
+		),
+
+		array(
+			'uid'     => 'storage_choice',
+			'label'   => 'STORAGE_CHOICE',
+			'default' => '1',
+			'section' => 'first_section',
+			'type'    => 'select',
+			'options' => array( 'IPFS' => __( 'IPFS', 'kredeum-nfts' ), 'SWARM' => __( 'Swarm', 'kredeum-nfts' ) ),
+			'helper'  => __( 'Choose on witch decentralized storage you want to upload your files', 'kredeum-nfts' ),
+		),
+	);
+	
+	if (defined('STORAGE_CHOICE')) {
+		switch (STORAGE_CHOICE) {
+			case "IPFS":
+				array_push(
+					$fields, 
+					array(
+						'uid'         => 'nft_storage_key',
+						'label'       => 'NFT_STORAGE_KEY',
+						'section'     => 'first_section',
+						'type'        => 'textarea',
+						'placeholder' => 'NFT Storage key',
+						'default'     => '',
+						'helper'      => __( 'Enter your own NFT Storage Key, or leave blank to use common key', 'kredeum-nfts' ),
+					),
+				);
+				break;
+			case "SWARM":
+				array_push(
+					$fields, 
+					array(
+						'uid'         => 'swarm_node_url',
+						'label'       => 'SWARM_NODE_URL',
+						'section'     => 'first_section',
+						'type'        => 'text',
+						'placeholder' => 'Your Swarm Bee node URL',
+						'default'     => '',
+						'helper'      => __( 'Enter your own Swarm Bee node Url (ex: http://localhost:1633), or leave blank to use Swarm free(limited) Gateway', 'kredeum-nfts' ),
+					),
+					array(
+						'uid'         => 'swarm_batch_id',
+						'label'       => 'SWARM_BATCH_ID',
+						'section'     => 'first_section',
+						'type'        => 'text',
+						'placeholder' => 'Your Swarm Bee Batch of stamps ID',
+						'default'     => '',
+						'helper'      => __( 'Enter your own Swarm Bee Batch of stamps ID, or leave blank to use Swarm free(limited) Gateway', 'kredeum-nfts' ),
+					),
+				);
+				break;
+		}
+	}
+	
+	 return $fields;
 }
