7031532cc6882dd5d019cc0a75ba9bd6fa31a897
zapaz.eth <alain@kredeum.com>
2023-04-19 20:53:06 +0200

    refacto handlebars

---
+++ common/build-handlebars.ts
@@ -0,0 +1,11 @@
+import { runHandlebarsEnv } from "@utils/runHandlebarsEnv";
+import { runHandlebarsConfig } from "@utils/runHandlebarsConfig";
+
+const main = async (): Promise<void> => {
+  await runHandlebarsEnv("common/config/config.handlebars.json", "common/config/config.json");
+  await runHandlebarsEnv("common/config/networks.handlebars.json", "common/config/networks.json");
+  await runHandlebarsConfig("wordpress/readme.txt_handlebars", "wordpress/plugins/kredeum-nfts/readme.txt");
+  await runHandlebarsConfig("wordpress/kredeum-nfts.php_handelbars", "wordpress/plugins/kredeum-nfts/kredeum-nfts.php");
+};
+
+main().catch(console.error);
--- common/utils/buildNetworks.ts
+++
@@ -1,7 +0,0 @@
-import { runHandlebars } from "@utils/runHandlebars";
-
-const buildNetworks = async (): Promise<void> => {
-  await runHandlebars(`${__dirname}/../config/networks.handlebars.json`, `${__dirname}/../config/networks.json`);
-};
-
-export { buildNetworks };
