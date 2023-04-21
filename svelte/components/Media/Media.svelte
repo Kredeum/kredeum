ed376d6e1fa5294fe31ed2e356478ca3d78866f9
zapaz.eth <alain@kredeum.com>
2023-04-21 15:04:59 +0200

    fix

--- svelte/components/Media/Media.svelte
+++ svelte/components/Media/Media.svelte
@@ -17,7 +17,7 @@
   $: nft = nftStore(chainId, address, tokenID);
   /////////////////////////////////////////////////
 
-  let cssMedia = mode === "line" ? "media-small" : mode === "zoom" ? "" : "media-grid";
+  let cssMedia = mode === "line" ? "media-small" : mode === "zoom" ? "full" : "media-grid";
   let gridScale = mode.startsWith("grid") ? " a-simul-cursor" : "";
 </script>
 
