1c5bc5b16ce240317ad439d8846a86f9efb021ed
dlaxcess <flipiste@free.fr>
2023-06-02 20:50:31 +0200

    get back getBee function

--- common/lib/common/beejs.ts
+++ common/lib/common/beejs.ts
@@ -1,6 +1,10 @@
 import { Bee } from "@ethersphere/bee-js";
-import { DEFAULT_NAME } from "@lib/common/config";
+import { DEFAULT_NAME, config } from "@lib/common/config";
 import { swarmApiEndpoint, swarmApiKey, SWARM_ZERO_APIKEY } from "@lib/nft/storage/swarm";
+
+const getBee = (nodeUrl: string): Bee => {
+  return new Bee(nodeUrl ? nodeUrl : config.storage.swarm.apiEndpoint);
+};
 
 const swarmUploadFile = async (file: File | string): Promise<string> => {
   const nodeUrl = swarmApiEndpoint();
@@ -8,17 +12,17 @@ const swarmUploadFile = async (file: File | string): Promise<string> => {
   // console.log("swarmUploadFile ~ batchId:", batchId);
   // console.log("swarmUploadFile ~ nodeUrl:", nodeUrl);
 
-  const bee: Bee = new Bee(nodeUrl);
+  const bee: Bee = getBee(nodeUrl);
 
   const isFile = file instanceof File;
-  const isUserBatchID = batchId && batchId !== SWARM_ZERO_APIKEY;
+  const isPinnableBatchID = batchId && batchId !== SWARM_ZERO_APIKEY;
 
   const result = await bee.uploadFile(
-    isUserBatchID ? batchId : SWARM_ZERO_APIKEY,
+    isPinnableBatchID ? batchId : SWARM_ZERO_APIKEY,
     file,
     isFile ? file.name : DEFAULT_NAME,
     {
-      pin: isUserBatchID ? true : false,
+      pin: isPinnableBatchID ? true : false,
       size: isFile ? file.size : undefined,
       contentType: isFile ? file.type : undefined
     }
