7e5e6101a3e0874b96387a823131031af46fadd4
dlaxcess <flipiste@free.fr>
2023-06-22 19:59:16 +0200

    wip

--- wordpress/plugins/kredeum-nfts/admin/post-list/actions.php
+++ wordpress/plugins/kredeum-nfts/admin/post-list/actions.php
@@ -29,23 +29,9 @@ add_filter(
 		$na = 0; // number of archived files.
 		$nm = 0; // number of modified files.
 		$nu = 0; // number of unchanged files.
+		
 		if ( 'kre-post-archive' === $action ) {
-			// foreach ( $post_ids as $post_id ) {
-			// 	$file = get_attached_file_meta( $post_id );
-			// 	$uri  = insert( $post_id );
-			// 	if ( $file->uri ) {
-			// 		if ( $file->uri === $uri ) {
-			// 			$nu++;
-			// 		} else {
-			// 			$nm++;
-			// 		}
-			// 	} else {
-			// 		$na++;
-			// 	}
-			// }
-			// $redirect_url = add_query_arg( 'bulk_archived', $na, $redirect_url );
-			// $redirect_url = add_query_arg( 'bulk_modified', $nm, $redirect_url );
-			// $redirect_url = add_query_arg( 'bulk_unchanged', $nu, $redirect_url );
+			
             
             foreach ( $post_ids as $post_id ) {
                 $post_data = get_post($post_id);
@@ -54,6 +40,10 @@ add_filter(
                 $post_author = get_the_author_meta('display_name', $post_data->post_author); // Get the author name
                 $post_date = get_the_date('', $post_id); // Get the publication date
 
+				// ini_set("xdebug.var_display_max_children", '-1');
+				// ini_set("xdebug.var_display_max_data", '-1');
+				// ini_set("xdebug.var_display_max_depth", '-1');
+				// var_dump($post_content); die();
                 // Get the featured image URL
                 $featured_image_url = '';
                 if (has_post_thumbnail($post_id)) {
@@ -62,14 +52,17 @@ add_filter(
                     $featured_image_url = $image_data[0];
                 }
                 // var_dump($post_content); die();
+				$font = 'helvetica';
+				$fontSize = 12;
                 
                 $pdf = new \TCPDF(); // Initialize TCPDF instance
                 $pdf->AddPage(); // Add a new page
-                $pdf->SetFont('times', '', 12); // Set font and font size
+                $pdf->SetFont($font, '', $fontSize); // Set font and font size
+				// $pdf->SetMargins(0, $pdf->GetCellHeight($font, $fontSize), 0);
                 
                 $header = '<h1>' . $post_title . '</h1>';
                 if ($featured_image_url) {
-                    $header .= '<img src="' . $featured_image_url . '">';
+                    $header .= '<div><img src="' . $featured_image_url . '"></div>';
                 }
                 $header .= '<p><strong>Author:</strong> ' . $post_author . '</p>';
                 $header .= '<p><strong>Date:</strong> ' . $post_date . '</p>';
@@ -110,8 +103,33 @@ add_filter(
 
                 // Display the attachment URL
                 $attachment_url = wp_get_attachment_url($attachment_id);
-                echo "PDF saved: <a href=\"$attachment_url\">$attachment_url</a>";
+                // echo "PDF saved: <a href=\"$attachment_url\">$attachment_url</a>";
+				// var_dump(get_attached_file($attachment_id)); die();
+				
+				$post_metadatas = wp_get_attachment_metadata($post_id);
+				$file = get_attached_file_meta($post_metadatas['pdf_id']);
+				$uri  = insert( $attachment_id );
+				var_dump($uri);
+				var_dump($file->uri);
+				die();
+				if ( $file->uri ) {
+					if ( $file->uri === $uri ) {
+						$nu++;
+					} else {
+						$nm++;
+					}
+				} else {
+					$na++;
+				}
+				
+				$post_metadatas = wp_get_attachment_metadata($post_id);
+				$post_metadatas['pdf_id'] = $attachment_id;
+				wp_update_attachment_metadata($post_id, $post_metadatas);
             }
+			
+			$redirect_url = add_query_arg( 'bulk_post_archived', $na, $redirect_url );
+			$redirect_url = add_query_arg( 'bulk_post_modified', $nm, $redirect_url );
+			$redirect_url = add_query_arg( 'bulk_post_unchanged', $nu, $redirect_url );
 		}
 		return $redirect_url;
 	},
@@ -125,34 +143,34 @@ add_filter(
 add_action(
 	'admin_notices',
 	function () {
-		if ( ! empty( $_REQUEST['bulk_archived'] ) ) {
-			$archived_count = intval( $_REQUEST['bulk_archived'] );
+		if ( ! empty( $_REQUEST['bulk_post_archived'] ) ) {
+			$archived_count = intval( $_REQUEST['bulk_post_archived'] );
 			printf(
 				'<div id="message" class="notice notice-success is-dismissible"><p>'
 				// translators: %s = $archived_count = number of medias archived.
-				. esc_html( _n( '%s media archived to Storage', '%s medias archived to Storage', $archived_count, 'kredeum-nfts' ) )
+				. esc_html( _n( '%s article archived to Storage', '%s articles archived to Storage', $archived_count, 'kredeum-nfts' ) )
 				. '</p></div>',
 				esc_html( $archived_count ),
 			);
 		}
-		if ( ! empty( $_REQUEST['bulk_modified'] ) ) {
-			$modified_count = intval( $_REQUEST['bulk_modified'] );
+		if ( ! empty( $_REQUEST['bulk_post_modified'] ) ) {
+			$modified_count = intval( $_REQUEST['bulk_post_modified'] );
 			printf(
 			// translators: must explain %1 %2.
 				'<div id="message" class="notice notice-warning is-dismissible"><p>'
 				// translators: %s = $modified_count = number of medias modified.
-				. esc_html( _n( '%s Storage media link modified', '%s Storage medias links modified', $modified_count, 'kredeum-nfts' ) )
+				. esc_html( _n( '%s Storage article link modified', '%s Storage articles links modified', $modified_count, 'kredeum-nfts' ) )
 				. '</p></div>',
 				esc_html( $modified_count )
 			);
 		}
-		if ( ! empty( $_REQUEST['bulk_unchanged'] ) ) {
-			$unchanged_count = intval( $_REQUEST['bulk_unchanged'] );
+		if ( ! empty( $_REQUEST['bulk_post_unchanged'] ) ) {
+			$unchanged_count = intval( $_REQUEST['bulk_post_unchanged'] );
 			printf(
 			// translators: must explain %1 %2.
 				'<div id="message" class="notice is-dismissible"><p>'
 				// translators: %s = $unchanged_count = number of medias unchanged.
-				. esc_html( _n( '%s Storage media link unchanged', '%s Storage medias links unchanged', $unchanged_count, 'kredeum-nfts' ) )
+				. esc_html( _n( '%s Storage article link unchanged', '%s Storage articles links unchanged', $unchanged_count, 'kredeum-nfts' ) )
 				. '</p></div>',
 				esc_html( $unchanged_count )
 			);
--- wordpress/plugins/kredeum-nfts/admin/post-list/column.php
+++ wordpress/plugins/kredeum-nfts/admin/post-list/column.php
@@ -18,10 +18,45 @@ add_filter('manage_posts_columns', function ($columns) {
 add_action(
 	'manage_posts_custom_column',
 	function ($column, $post_id) {
-		if ($column === 'kre-post-archive') {
-			echo '<a href="#" class="button">Archive</a>';
+		if ($column === 'kre-post-uri') {
+			$post_metadatas = wp_get_attachment_metadata($post_id);
+			
+			if($post_metadatas && $post_metadatas['pdf_id']) {
+				$pdf = get_post($post_metadatas['pdf_id']);
+				// var_dump($pdf->_kre_uri); //die();
+				echo wp_kses( link( $pdf->_kre_uri, short_uri( $pdf->_kre_uri ) ), array( 'a' => array( 'href' => array() ) ) );
+			}
+			// echo '<a href="#" class="button">Archive</a>';
 		}
 	},
 	10,
 	2
+	// function ( $name ) {
+	// 	global $post;
+
+	// 	if ( 'kre-post-uri' === $name ) {
+	// 		if ( $post->_kre_uri ) {
+	// 			echo wp_kses( link( $post->_kre_uri, short_uri( $post->_kre_uri ) ), array( 'a' => array( 'href' => array() ) ) );
+	// 		}
+	// 	}
+
+	// 	// if ( 'kre-nft' === $name ) {
+	// 	// 	$nid = '';
+	// 	// 	if ( $post->_kre_nid ) {
+	// 	// 		$nid = $post->_kre_nid;
+	// 	// 	}
+
+	// 	// 	$metadata = get_metadata( 'post', $post->ID );
+
+	// 	// 	printf(
+	// 	// 		'<div class="kredeum-mint-button" txt="true"'
+	// 	// 		. ' src="' . esc_attr( wp_get_attachment_url( $post->ID ) ) . '"'
+	// 	// 		. ' pid="' . esc_attr( $post->ID ) . '"'
+	// 	// 		. ' nid="' . esc_attr( $nid ) . '"'
+	// 	// 		. ' metadata="' . esc_attr( wp_json_encode( $metadata ) ) . '"'
+	// 	// 		. ' alt="' . esc_attr( $post->post_title ) . '"'
+	// 	// 		. '/>'
+	// 	// 	);
+	// 	// }
+	// }
 );
