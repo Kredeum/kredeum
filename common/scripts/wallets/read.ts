76a44826b40fda0d5b01730bdde6234811508e39
zapaz.eth <alain@kredeum.com>
2023-07-17 11:05:14 +0200

    small script adjustments

--- common/scripts/wallets/read.ts
+++ common/scripts/wallets/read.ts
@@ -1,10 +1,15 @@
 import { Wallet } from "ethers";
+import fs from "fs";
 
-const password = process.env.PASSWORD || "";
-const json = process.env.JSON || "";
+if (process.argv.length < 3) {
+  console.log("syntaxe : ts-node read.ts <file.json> [password]");
+  process.exit(1);
+}
+
+const json = fs.readFileSync(process.argv[2], "utf8");
+const password =  (process.argv.length >= 4) ? process.argv[3] : "";
 
 const wallet = Wallet.fromEncryptedJsonSync(json, password);
 console.log(wallet.address);
 console.log(wallet.privateKey);
-// console.log(wallet.mnemonic.phrase);
-// console.log(wallet.mnemonic.path);
+console.log(wallet.mnemonic);
