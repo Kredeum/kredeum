abda2375518238af134e0e05a80ff272a3d630c7
zapaz.eth <alain@kredeum.com>
2024-01-25 19:05:59 +0100

    deploy on skale testnet

--- svelte/components/Network/Network.svelte
+++ svelte/components/Network/Network.svelte
@@ -14,13 +14,13 @@
     str && str.length >= 1 ? str.charAt(0).toUpperCase() + str.substring(1) : "";
 
   $: chainName = networks.getChainName(chainId);
-  $: mainnetName = networks.isTestnet(chainId) ? networks.getChainName(networks.getLinkedMainnet(chainId)) : chainName;
+  $: iconName = networks.getIconName(chainId);
   $: chainLabel = strUpFirst(chainName);
   $: preLabel = pre ? (networks.isLayer2(chainId) ? "L2 " : "") : "";
 
   $: logoPos = pre ? (networks.isLayer2(chainId) ? 25 : 0) : 0;
 
-  $: logoStyle = `background: url("./assets/images/icon-${mainnetName}.png") no-repeat ${logoPos}px center/25px; padding: 5px 0;`;
+  $: logoStyle = `background: url("./assets/images/${iconName}") no-repeat ${logoPos}px center/25px; padding: 5px 0;`;
   $: spacerStyle = `margin-left: 35px;`;
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
