e08851b9a4e297a341554220c73ed16678c9d1cf
dlaxcess <flipiste@free.fr>
2024-02-21 17:58:50 +0100

    All logic in config helper

--- svelte/src/components/Input/UserConfig/InputConfigSections.svelte
+++ svelte/src/components/Input/UserConfig/InputConfigSections.svelte
@@ -1,76 +1,18 @@
 <script lang="ts">
+  import type { UserConfig } from "@svelte/helpers/configHelper";
   import { onMount } from "svelte";
 
-  import config from "@kredeum/config/dist/config.json";
-  import {
-    localConfigSet,
-    mapStringify,
-    jsonToMap,
-    isUrlValid,
-    isBatchIdValid,
-    localConfigImport
-  } from "@svelte/helpers/configHelper";
+  import { storageConfigInit, checkAndSave } from "@svelte/helpers/configHelper";
   import InputConfigFields from "./InputConfigFields.svelte";
 
-  let userConfig: { [key: string]: Map<string, any> } = {};
+  let userConfig: UserConfig = {};
 
   onMount(() => {
-    userConfig.storage = jsonToMap(config.storage);
-
-    userConfig = localConfigImport(userConfig);
+    userConfig = storageConfigInit(userConfig);
   });
 
-  ///////////////////////////////////////
   const saveUserConfig = () => {
-    if (typeof localStorage !== "undefined") {
-      deleteFieldErrors();
-
-      Object.entries(userConfig).forEach(([namespace, configSection]) => {
-        if (isSectionValid(namespace, configSection)) {
-          localConfigSet(namespace, mapStringify(configSection));
-        }
-      });
-    }
-  };
-
-  ///////////////////////////////////////
-  const isSectionValid = (namespace: string, configSection: Map<string, any>) => {
-    if (namespace === "storage") {
-      configSection.forEach((value, key) => {
-        if (key !== "errors" && typeof value === "object") {
-          Object.entries(value).forEach(([storageParamKey, storageParamValue]) => {
-            if (storageParamKey === "gateway" || storageParamKey === "apiEndpoint") {
-              if (!isUrlValid(storageParamValue as string)) {
-                addFieldError(namespace, key, storageParamKey, "Bad URL");
-              }
-            } else if (key === "swarm" && storageParamKey === "apiKey") {
-              if (!isBatchIdValid(storageParamValue as string)) {
-                addFieldError(namespace, key, storageParamKey, "Invalid BatchId");
-              }
-            }
-          });
-        }
-      });
-    }
-
-    return !userConfig[namespace].get("errors");
-  };
-
-  const addFieldError = (namespace: string, storageType: string, key: string, errMessage: string) => {
-    let section = userConfig[namespace];
-    let errors = section.get("errors") || new Map();
-    let storageErrors = errors.get(storageType) || new Map();
-
-    storageErrors.set(key, errMessage);
-    errors.set(storageType, storageErrors);
-    section.set("errors", errors);
-
-    userConfig[namespace] = section;
-  };
-
-  const deleteFieldErrors = () => {
-    Object.values(userConfig).forEach((configSection) => configSection.delete("errors"));
-    userConfig = { ...userConfig };
+    userConfig = checkAndSave(userConfig);
   };
 </script>
 
--- svelte/src/helpers/configHelper.ts
+++ svelte/src/helpers/configHelper.ts
@@ -1,39 +1,107 @@
+import config from "@kredeum/config/dist/config.json";
+
+type UserConfig = {
+  [key: string]: ConfigSection
+}
+
+type ConfigSection = Map<
+  string,
+  string | object
+> | Map<"errors", SectionErrors>;
+
+type SectionErrors = Map<string, Map<string, Map<string, string>>>
+
 const krdNamespace = "kredeum";
 
 const localConfigGetKey = (key: string) => key.replace(`${krdNamespace}.`, "");
 
 const localConfigSet = (namespaceKey: string, value: string): void =>
-    localStorage?.setItem(`${krdNamespace}.${namespaceKey}`, value);
+  localStorage?.setItem(`${krdNamespace}.${namespaceKey}`, value);
 
-const mapStringify = (map: Map<string, string | object>): string => JSON.stringify(Object.fromEntries(map));
-const jsonToMap = (json: object): Map<string, string | object> => new Map(Object.entries(json));
+const mapSectionStringify = (map: ConfigSection): string => JSON.stringify(Object.fromEntries(map));
+const jsonToMapSection = (json: object): ConfigSection => new Map(Object.entries(json));
+
+/////////////////////////////////////////
+const storageConfigInit = (userConfig: UserConfig) => {
+  userConfig.storage = jsonToMapSection(config.storage);
+  return localConfigImport(userConfig);
+}
+
+const localConfigImport = (userConfig: UserConfig) => {
+  if (typeof localStorage !== "undefined") {
+    Object.entries(localStorage)
+      .filter(([namespaceKey,]) => namespaceKey.startsWith(`${krdNamespace}.`))
+      .forEach(([namespaceKey, localConfigSection]) => {
+        const namespace = localConfigGetKey(namespaceKey);
+        const localSectionMap = jsonToMapSection(JSON.parse(localConfigSection));
+
+        userConfig[namespace] = localSectionMap;
+      });
+  }
+  return userConfig
+};
+
+/////////////////////////////////////////
+const checkAndSave = (userConfig: UserConfig) => {
+  if (typeof localStorage !== "undefined") {
+    deleteFieldErrors(userConfig);
+
+    Object.entries(userConfig).forEach(([namespace, configSection]) => {
+      if (isSectionValid(namespace, configSection)) {
+        localConfigSet(namespace, mapSectionStringify(configSection));
+      }
+    });
+  }
+
+  return userConfig
+};
+
+/////////////////////////////////////////
+const isSectionValid = (namespace: string, configSection: ConfigSection) => {
+  if (namespace === "storage") {
+    configSection.forEach((value, key) => {
+      if (key !== "errors" && typeof value === "object") {
+        Object.entries(value).forEach(([storageParamKey, storageParamValue]) => {
+          if (storageParamKey === "gateway" || storageParamKey === "apiEndpoint") {
+            if (!isUrlValid(storageParamValue as string)) {
+              addFieldError(configSection, key, storageParamKey, "Bad URL");
+            }
+          } else if (key === "swarm" && storageParamKey === "apiKey") {
+            if (!isBatchIdValid(storageParamValue as string)) {
+              addFieldError(configSection, key, storageParamKey, "Invalid BatchId");
+            }
+          }
+        });
+      }
+    });
+  }
+
+  return !configSection.get("errors");
+};
 
 const isUrlValid = (url: string): boolean => {
-    if (!url) return false;
-    try {
-        new URL(url);
-        return true;
-    } catch (e) {
-        return false;
-    }
+  if (!url) return false;
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
 };
 
 const isBatchIdValid = (batchId: string | undefined): boolean => Boolean(batchId?.replace(/^0x/, "").length === 64);
 
-/////////////////////////////////////////
+///////////////////////////////////////////////
+const addFieldError = (section: ConfigSection, storageType: string, key: string, errMessage: string) => {
+  const errors: SectionErrors = section.get("errors") as SectionErrors || new Map();
+  const storageErrors = errors.get(storageType) || new Map();
 
-const localConfigImport = (userConfig: { [key: string]: Map<string, string|object> }) => {
-    if (typeof localStorage !== "undefined") {
-      Object.entries(localStorage)
-        .filter(([namespaceKey, _]) => namespaceKey.startsWith(`${krdNamespace}.`))
-        .forEach(([namespaceKey, localConfigSection]) => {
-          const namespace = localConfigGetKey(namespaceKey);
-          const localSectionMap = jsonToMap(JSON.parse(localConfigSection));
+  storageErrors.set(key, errMessage);
+  errors.set(storageType, storageErrors);
+  section.set("errors", errors);
+};
 
-          userConfig[namespace] = localSectionMap;
-        });
-    }
-    return userConfig
-  };
+const deleteFieldErrors = (userConfig: UserConfig) => Object.values(userConfig).forEach((configSection) => configSection.delete("errors"));
 
-export { krdNamespace, localConfigGetKey, localConfigSet, mapStringify, jsonToMap, isUrlValid, isBatchIdValid, localConfigImport };
+export type { UserConfig };
+export { storageConfigInit, checkAndSave };
