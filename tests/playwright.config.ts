4f34fad33400d9ec1f73668f4c2f9b7761b73fb1
dlaxcess <flipiste@free.fr>
2024-04-04 18:43:38 +0200

    dAppwright WIP

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,13 +1,54 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import dappwright, { Dappwright, MetaMaskWallet } from '@tenkeylabs/dappwright';
+
 
 const config: PlaywrightTestConfig = {
+	workers: 1,
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		// url: "http://localhost:4173",
+		// reuseExistingServer: true,
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	outputDir: "test-results",
+	use: {
+		// Configurez le navigateur pour qu'il ne soit pas en mode headless
+		headless: false,
+	},
+	// projects: [
+	// 	{
+	// 		name: 'dAppwright',
+	// 		use: {
+	// 			context: async ({ }, use) => {
+	// 				// Launch context with extension
+	// 				const [wallet, _, context] = await dappwright.bootstrap("", {
+	// 					wallet: "metamask",
+	// 					version: MetaMaskWallet.recommendedVersion,
+	// 					seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
+	// 					headless: false,
+	// 				});
+
+	// 				// Add Hardhat as a custom network
+	// 				await wallet.addNetwork({
+	// 					networkName: "sepolia",
+	// 					rpc: "https://sepolia.infura.io/v3/",
+	// 					chainId: 11155111,
+	// 					symbol: "SepoliaETH",
+	// 				});
+
+	// 				await use(context);
+	// 			},
+
+	// 			wallet: async ({ context }, use) => {
+	// 				const metamask = await dappwright.getWallet("metamask", context);
+
+	// 				await use(metamask);
+	// 			},
+	// 		}
+	// 	}
+	// ]
 };
 
 export default config;
--- tests/tests/test.ts
+++ tests/tests/test.ts
@@ -1,26 +1,121 @@
 // import { expect, test } from '@playwright/test';
-import { test, expect } from "../synpressMetamaskTestFitures";
-import * as metamask from "@synthetixio/synpress/commands/metamask";
+import { type BrowserContext, expect, test as baseTest, chromium, firefox, webkit } from "@playwright/test";
 import { type Page } from "@playwright/test";
 
-let sharedPage: Page;
+import path from 'path';
 
-test.describe.configure({ mode: "serial" });
+import dappwright, { type Dappwright, MetaMaskWallet } from "@tenkeylabs/dappwright";
 
-test.beforeAll(async ({ page }) => {
-	sharedPage = page;
-	await sharedPage.goto("/metamask");
+export const test = baseTest.extend<{
+	context: BrowserContext;
+	wallet: Dappwright;
+	// globalContext: BrowserContext;
+}>({
+	// globalContext: [async ({ browserName }, use) => {
+	// 	const browserTypes = { chromium, firefox, webkit };
+	// 	const context = await browserTypes[browserName].launchPersistentContext(path.join(__dirname, 'ppp'), {});
+	// 	await use(context);
+	// 	await context.close();
+	// }, { scope: 'test' }],
+
+	// page: async ({ globalContext }, use) => {
+	// 	const page = await globalContext.newPage();
+	// 	await use(page);
+	// 	await page.close();
+	// },
+	
+
+	context: async ({ }, use) => {
+		// Launch context with extension
+		const [wallet, _, context] = await dappwright.bootstrap("", {
+			wallet: "metamask",
+			version: MetaMaskWallet.recommendedVersion,
+			seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
+			headless: false,
+		});
+
+		// Add Hardhat as a custom network
+		await wallet.addNetwork({
+			networkName: "sepolia",
+			rpc: "https://sepolia.infura.io/v3/",
+			chainId: 11155111,
+			symbol: "SepoliaETH",
+		});
+
+		await use(context);
+	},
+
+	wallet: async ({ context }, use) => {
+		const metamask = await dappwright.getWallet("metamask", context);
+
+		await use(metamask);
+	},
+
 });
 
-test.afterAll(async ({ }) => {
-	await sharedPage.close();
+// test.beforeEach(async ({ page }) => {
+// 	await page.goto("http://localhost:4173");
+// });
+
+test("should be able to connect", async ({ wallet, page }) => {
+	// test("should be able to connect", async ({ page }) => {
+
+	// const [wallet, _, context] = await dappwright.bootstrap("", {
+	// 	wallet: "metamask",
+	// 	version: MetaMaskWallet.recommendedVersion,
+	// 	seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
+	// 	headless: false,
+	// });
+
+	// // Add Hardhat as a custom network
+	// await wallet.addNetwork({
+	// 	networkName: "sepolia",
+	// 	rpc: "https://sepolia.infura.io/v3/",
+	// 	chainId: 11155111,
+	// 	symbol: "SepoliaETH",
+	// });
+
+	await page.goto("http://localhost:4173/metamask");
+
+
+
+	// const metamask = await dappwright.getWallet("metamask", context);
+
+	await wallet.switchNetwork('sepolia');
+
+
+	await page.click("#connect-metamask-button");
+	// await wallet.approve();
+
+	// const connectStatus = page.getByTestId("connect-status");
+	// expect(connectStatus).toHaveValue("connected");
+
+	// await page.click("#switch-network-button");
+
+	// const networkStatus = page.getByTestId("network-status");
+	// expect(networkStatus).toHaveValue("31337");
 });
 
-test('index page has expected h1', async ({ page }) => {
-	// await page.goto('/metamask');
-	await expect(page.getByRole('heading', { name: 'Test MetaMask Connexion' })).toBeVisible();
-});
+// let sharedPage: Page;
 
+// test.describe.configure({ mode: "serial" });
+
+// test.beforeAll(async ({ page }) => {
+// 	sharedPage = page;
+// 	await sharedPage.goto("/metamask");
+// });
+
+// test.afterAll(async ({ }) => {
+// 	await sharedPage.close();
+// });
+
+// test('index page has expected h1', async ({ page }) => {
+// 	await expect(page.getByRole('heading', { name: 'Test MetaMask Connexion' })).toBeVisible();
+// });
+
+
+
+/////////////////////////////////////////////////////////
 // test('connexion to Metamask ok', async ({ page }) => {
 // 	await page.goto('/metamask');
 // 	await page.click('#connect-metamask-button');
@@ -41,22 +136,22 @@ test('index page has expected h1', async ({ page }) => {
 
 
 
-test("connect wallet using default metamask account", async () => {
-	await sharedPage.click("#connect-metamask-button");
-	await metamask.acceptAccess();
-	await expect(sharedPage.locator("#accounts")).toHaveText(
-		"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-	);
-});
+// test("connect wallet using default metamask account", async () => {
+// 	await sharedPage.click("#connect-metamask-button");
+// 	await metamask.acceptAccess();
+// 	await expect(sharedPage.locator("#accounts")).toHaveText(
+// 		"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
+// 	);
+// });
 
-test("import private key and connect wallet using imported metamask account", async () => {
-	await metamask.disconnectWalletFromAllDapps();
-	await metamask.importAccount(
-		"0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97"
-	);
-	await sharedPage.click("#connect-metamask-button");
-	await metamask.acceptAccess();
-	await expect(sharedPage.locator("#accounts")).toHaveText(
-		"0x23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"
-	);
-});
+// test("import private key and connect wallet using imported metamask account", async () => {
+// 	await metamask.disconnectWalletFromAllDapps();
+// 	await metamask.importAccount(
+// 		"0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97"
+// 	);
+// 	await sharedPage.click("#connect-metamask-button");
+// 	await metamask.acceptAccess();
+// 	await expect(sharedPage.locator("#accounts")).toHaveText(
+// 		"0x23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"
+// 	);
+// });
