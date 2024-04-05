b371726e0ad96f9a972f686c89b978a04af814d6
dlaxcess <flipiste@free.fr>
2024-04-05 14:56:15 +0200

    clean

--- skale/src/skaleReceiveFunds.ts
+++ skale/src/skaleReceiveFunds.ts
@@ -3,6 +3,7 @@ import {
   type WalletClient,
   type TransactionRequestLegacy,
   type TransactionReceipt,
+  type Address,
   http,
   createPublicClient,
   createWalletClient,
@@ -24,7 +25,7 @@ import {
 import mineGasForTransaction from "./skale-miner";
 import { scaleFaucets } from "./skaleFaucets";
 
-let _sessionPrivateKey: `0x${string}`;
+let _sessionPrivateKey: Address;
 
 ////////////////////////////////////////////////
 const chains = [
@@ -48,68 +49,62 @@ const _getChainFromId = (chainId: number): ChainType => {
   });
 };
 
-const _faucetAddressGet = (chainId: number): `0x${string}` => scaleFaucets[chainId].proofOfWork;
+const _faucetAddressGet = (chainId: number): Address => scaleFaucets[chainId].proofOfWork;
 const _faucetThresholdGet = (chainId: number): bigint => parseEther(String(scaleFaucets[chainId].threshold));
 
-const _functionSignatureGet = (chainId: number): `0x${string}` => {
+const _functionSignatureGet = (chainId: number): Address => {
   const functionSignature = chainId === skaleEuropa.id ? "0x6a627842" : "0x0c11dedd";
   return `${functionSignature}000000000000000000000000`;
 };
 const _rmBytesSymbol = (address: string) => address.replace(/^0x/, "");
 
 ////////////////////////////////////////////////
-const receiveFunds = async (account: `0x${string}`, chainId: number): Promise<TransactionReceipt | undefined> => {
-  try {
-    if (!account) throw new Error("No account provided");
-    if (!chainsId.includes(chainId as ChainIdsType)) throw new Error("You Must be on a Skale chain");
+const receiveFunds = async (account: Address, chainId: number): Promise<TransactionReceipt | undefined> => {
+  if (!account) throw new Error("No account provided");
+  if (!chainsId.includes(chainId as ChainIdsType)) throw new Error("You Must be on a Skale chain");
 
-    const chain = _getChainFromId(chainId);
-    const publicClient: PublicClient = createPublicClient({ chain, transport: http() });
+  const chain = _getChainFromId(chainId);
+  const publicClient: PublicClient = createPublicClient({ chain, transport: http() });
 
-    const receiverBalance = await publicClient.getBalance({ address: account });
-    if (receiverBalance >= _faucetThresholdGet(chainId)) return;
+  const receiverBalance = await publicClient.getBalance({ address: account });
+  if (receiverBalance >= _faucetThresholdGet(chainId)) return;
 
-    if (!_sessionPrivateKey) {
-      _sessionPrivateKey = generatePrivateKey();
-    }
-
-    const signer = privateKeyToAccount(_sessionPrivateKey);
-    const sessionAccount = signer.address;
-    // console.info("sessionAccount:", sessionAccount);
-
-    const walletClient: WalletClient = createWalletClient({ account: sessionAccount, chain, transport: http() });
-    const nonce = await publicClient.getTransactionCount({ address: sessionAccount });
-
-    let tx: TransactionRequestLegacy = {
-      from: sessionAccount,
-      to: _faucetAddressGet(chainId),
-      data: (_functionSignatureGet(chainId) + _rmBytesSymbol(account)) as `0x${string}`,
-      nonce: nonce
-    };
-    const gas = await publicClient.estimateGas(tx);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { duration, gasPrice } = await mineGasForTransaction(nonce, Number(gas), sessionAccount);
-    // console.info("POW duration:", duration);
-    tx = { ...tx, gas, gasPrice };
-    // console.info("Prepared ~ Legacy Tx:", tx);
-
-    const signedTx = await signer.signTransaction(tx);
-    const hash = await walletClient.sendRawTransaction({
-      serializedTransaction: signedTx
-    });
-    console.info("Tx ~ hash: ", `${hash} => ${scaleFaucets[chainId].explorer}/tx/${hash}`);
-
-    const transactionReceipt = await publicClient.waitForTransactionReceipt({
-      hash: hash
-    });
-    console.info("TransactionReceipt:", transactionReceipt);
-
-    return transactionReceipt;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw error;
-    }
+  if (!_sessionPrivateKey) {
+    _sessionPrivateKey = generatePrivateKey();
   }
+
+  const signer = privateKeyToAccount(_sessionPrivateKey);
+  const sessionAccount = signer.address;
+  // console.info("sessionAccount:", sessionAccount);
+
+  const walletClient: WalletClient = createWalletClient({ account: sessionAccount, chain, transport: http() });
+  const nonce = await publicClient.getTransactionCount({ address: sessionAccount });
+
+  let tx: TransactionRequestLegacy = {
+    from: sessionAccount,
+    to: _faucetAddressGet(chainId),
+    data: (_functionSignatureGet(chainId) + _rmBytesSymbol(account)) as Address,
+    nonce: nonce
+  };
+  const gas = await publicClient.estimateGas(tx);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { duration, gasPrice } = await mineGasForTransaction(nonce, Number(gas), sessionAccount);
+  // console.info("POW duration:", duration);
+  tx = { ...tx, gas, gasPrice };
+  // console.info("Prepared ~ Legacy Tx:", tx);
+
+  const signedTx = await signer.signTransaction(tx);
+  const hash = await walletClient.sendRawTransaction({
+    serializedTransaction: signedTx
+  });
+  console.info("Tx ~ hash: ", `${hash} => ${scaleFaucets[chainId].explorer}/tx/${hash}`);
+
+  const transactionReceipt = await publicClient.waitForTransactionReceipt({
+    hash: hash
+  });
+  console.info("TransactionReceipt:", transactionReceipt);
+
+  return transactionReceipt;
 };
 
 export { receiveFunds };
