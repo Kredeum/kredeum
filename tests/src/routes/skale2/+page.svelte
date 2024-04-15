76bde44164d09d042dfc7c893fbda336112f912f
zapaz.eth <alain@kredeum.com>
2024-04-15 11:00:50 +0200

    remove tests package

--- tests/src/routes/skale2/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { receiveFunds } from '@skale/skaleReceiveFunds';
-
-	receiveFunds('', 1564830818);
-</script>
-
-<h1>Skale faucet</h1>
-
-<p><a href="..">Home</a></p>
--- tests/tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page title ok', async ({ page }) => {
-	await page.goto('/');
-  await expect(page).toHaveTitle('Playwright Tests');
-});
