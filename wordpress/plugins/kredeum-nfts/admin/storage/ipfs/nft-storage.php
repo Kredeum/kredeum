fa55ea0476b20634d9c7aebbb0ccd0ba2b2ed890
dlaxcess <flipiste@free.fr>
2024-10-23 23:34:48 +0200

    restore original nft-storage.php deprecated file

--- wordpress/plugins/kredeum-nfts/admin/storage/ipfs/nft-storage.php
+++ wordpress/plugins/kredeum-nfts/admin/storage/ipfs/nft-storage.php
@@ -15,7 +15,7 @@ namespace KredeumNFTs\Storage;
  * @return string CID hash
  */
 function nft_storage_add_and_pin_dir( $attachment_id ) {
-	$api = new \RestClient( array( 'base_url' => IPFS_ENDPOINT ) );
+	$api = new \RestClient( array( 'base_url' => NFT_STORAGE_ENDPOINT ) );
 
 	$file     = file_get_contents( get_attached_file( $attachment_id ) );
 	$filename = get_attached_file_meta( $attachment_id )->filename;
@@ -30,60 +30,37 @@ function nft_storage_add_and_pin_dir( $attachment_id ) {
 	);
 	$buffer   = multipart( $parts, $boundary );
 	$headers  = array(
-		'Authorization'  => 'Bearer ' . IPFS_STORAGE_KEY,
+		'Authorization'  => 'Bearer ' . NFT_STORAGE_KEY,
 		'Content-Type'   => 'multipart/form-data; boundary=' . $boundary,
 		'Content-Length' => strlen( $buffer ),
 	);
 
-	$result = $api->post( '/', $buffer, $headers );
+	$result = $api->post( '/upload', $buffer, $headers );
 
 	// var_dump($result->decode_response()->value->cid);
 	// var_dump($result->decode_response()->value->files[0]->name);
 	// .
 
 	return ( 200 === $result->info->http_code ) ?
-	$result->decode_response()->value->IpfsHash . '/' .
+	$result->decode_response()->value->cid . '/' .
 	$result->decode_response()->value->files[0]->name
 	: $result->error;
 }
 
-	/**
-	 * IPFS add and pin file with Pinata
-	 *
-	 * @param string $file_id id of file to upload.
-	 *
-	 * @return string CID hash
-	 */
-function nft_storage_add_and_pin( $file_id ) {
-	if ( defined( 'IPFS_ENDPOINT' ) ) {
-		$api = new \RestClient( array( 'base_url' => IPFS_ENDPOINT ) );
+/**
+ * IPFS add and pin file with NFT Storage
+ *
+ * @param string $file file to upload.
+ *
+ * @return string CID hash
+ */
+function nft_storage_add_and_pin( $file ) {
+	if ( defined( 'NFT_STORAGE_ENDPOINT' ) ) {
+		$api     = new \RestClient( array( 'base_url' => NFT_STORAGE_ENDPOINT ) );
+		$headers = array( 'Authorization' => 'Bearer ' . NFT_STORAGE_KEY );
 
-		$file_content     = file_get_contents( get_attached_file( $file_id ) );
-		$filename = get_attached_file_meta( $file_id )->filename;
+		$result = $api->post( '/upload', $file, $headers );
 
-		$boundary = md5( rand() );
-		$buffer = "--{$boundary}\r\n";
-		$buffer .= "Content-Disposition: form-data; name=\"file\"; filename=\"" . $filename . "\"\r\n";
-		$buffer .= "Content-Type: application/octet-stream\r\n\r\n";
-		$buffer .= $file_content . "\r\n";
-
-		$buffer .= "--{$boundary}\r\n";
-		$buffer .= "Content-Disposition: form-data; name=\"pinataOptions\"\r\n\r\n";
-		$buffer .= json_encode( [
-			"cidVersion" => 1
-		] ) . "\r\n";
-
-		$buffer .= "--{$boundary}--\r\n";
-
-		$headers  = array(
-			'Authorization'  => 'Bearer ' . IPFS_STORAGE_KEY,
-			'Content-Type'   => 'multipart/form-data; boundary=' . $boundary,
-		);
-
-		$result = $api->post( '/', $buffer, $headers );
-
-		// var_dump( $result->decode_response()->IpfsHash ); die();
-
-		return ( 200 === $result->info->http_code ) ? $result->decode_response()->IpfsHash : $result->error;
+		return ( 200 === $result->info->http_code ) ? $result->decode_response()->value->cid : $result->error;
 	}
 }
