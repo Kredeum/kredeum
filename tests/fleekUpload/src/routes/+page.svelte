eabca5c64e2e99a6e713e99e18add39516398dbd
zapaz.eth <alain@kredeum.com>
2024-10-26 09:47:19 +0200

    tests fleek upload bof

---
+++ tests/fleekUpload/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fleek } from '$lib/fleek';
+
+	onMount(async () => {
+		const projects = await fleek.projects();
+		console.log(projects);
+	});
+</script>
+
+OK
